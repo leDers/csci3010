5cdd053f8cd885f2dae228d1ff7fac2e59157001
leDers <lean7049@colorado.edu>
2023-01-31 13:17:29 -0700

    lecture5

---
+++ lecture/lecture5/run_library.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include "Library.h"
+
+// Name(s):
+//
+// Complete numbered coding tasks.
+// Answer the questions with "Answer: " in the comments by writing your answer in the comments.
+// some experiments might result in non-running/non-compiling code. Leave these experiments in, but
+// comment them out.
+// Turn in run_library.cpp, Library.h, and Library.cpp on Canvas before the end of class.
+// make sure to leave a comment here and on canvas with your name and the name(s) of your partners.
+
+int main() {
+    // 1) Instantiate a couple Books. Set their fields equal to some values.
+    // Print out the values of the books' fields.
+
+
+    // 2) Instantiate a Library.
+
+    // 3) Add a void Donate(Book b) method to your Library class.
+    // Your Library should store the new book at the end of its shelf.
+
+
+    // 4) Donate one of the Books that you instantiated to your Library.
+
+
+    // 5) Print out the books in the Library (use the given PrintBooks method)
+
+
+    // 6) Change the title and/or author of the book that you donated to
+    // something different.
+    // Print out the new book information, then print out the books in your
+    // Library. Did changing the Book information change it in your Library?
+    //
+    // Answer (and your hypothesis for why/why not):
+
+
+    // 7) Create two Books that are separate structs (not pointers or references)
+    // but that have the same information.
+    // use the == operator to test if they are equal. What happens?
+    // if it succeeds, are these equal?
+    //
+    // Answer:
+
+    // 8) Create two Book references to your Books from # 7.
+    // use the == operator to test if they are equal. What happens?
+    // if it succeeds, are these equal?
+    //
+    // Answer:
+
+    // 9) Create two Book pointers to your Books from # 7.
+    // use the == operator to test if they are equal. What happens?
+    // if it succeeds, are these equal?
+    //
+    // Answer:
+
+
+    // 10) Add a void Donate(Book b, int num_copies) method to your Library class.
+    // your Library should add num_copies of Book b to its shelf.
+
+
+    // 11) Finally, if you reach this far, work on finding a solution to making it so
+    // that you would be able to do any of the comparisons from # 7, 8, and 9
+}

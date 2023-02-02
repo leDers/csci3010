f062499e39e1b041e26b3dd390b8be29b1997e76
leDers <lean7049@colorado.edu>
2023-02-02 12:37:32 -0700

    update directory names

---
+++ lecture/lecture5-6/Suit.h
@@ -0,0 +1,14 @@
+#ifndef _SUIT_H_ // this is the header guard
+#define _SUIT_H_
+
+// Create an enum class, Suit to represent the suit in a deck of cards
+
+// Create a struct, Card, to represent a card in a deck of cards.  You must
+// use your Suit enum.  You may use any other values that you choose.
+
+// Override operator== in your Card struct.
+
+// Design a class, Deck.  Write down the fields and methods that this class should have.  
+// Don’t implement them!
+
+#endif // _SUIT_H_

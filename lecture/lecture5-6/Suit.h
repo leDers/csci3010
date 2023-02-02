7c943706f5a6171720b1277780516b501f37ae69
leDers <lean7049@colorado.edu>
2023-02-02 12:58:47 -0700

    lecture 5-6

--- lecture/lecture5-6/Suit.h
+++ lecture/lecture5-6/Suit.h
@@ -2,13 +2,32 @@
 #define _SUIT_H_
 
 // Create an enum class, Suit to represent the suit in a deck of cards
+enum class suit {clubs, diamonds, hearts, spades};
 
 // Create a struct, Card, to represent a card in a deck of cards.  You must
 // use your Suit enum.  You may use any other values that you choose.
+struct Card{
+    suit s; // suit
+    int v;  // value
 
-// Override operator== in your Card struct.
+    // Override operator== in your Card struct.
+    const inline bool operator==(const Card& other){
+        return (s == other.s) && (v == other.v);
+    }
+};
 
 // Design a class, Deck.  Write down the fields and methods that this class should have.  
 // Don’t implement them!
+
+class Deck {
+    public:
+        Deck();
+
+        void shuffle();
+        void draw();
+
+    private:
+        std::vector<Card> cards_;
+};
 
 #endif // _SUIT_H_

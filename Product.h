75abede42f7d68d1167b43f4137e77228a6ac496
leDers <lean7049@colorado.edu>
2023-03-10 12:50:05 -0700

    resolved Product.h compile issues

--- Product.h
+++ Product.h
@@ -27,6 +27,7 @@ class Product {
         User *buyer;
         Product *p;
       };
+
       const std::string &GetProductName() const;
       const std::vector<Product::Bid> &GetListOfBids() const;
       double GetBasePrice() const;
@@ -35,9 +36,16 @@ class Product {
       bool IsActive() const;
       void AddBid(Product::Bid &b);
       void SetActive(bool active);
+
       Product(const std::string &product_name,
               const std::vector<Bid> &list_of_bids, double base_price,
-              unsigned long listing_id, Quality quality, bool active);
+              unsigned long listing_id, Quality quality, bool active){
+                this->productName_ = product_name;
+                this->listOfBids_ = list_of_bids;
+                this->basePrice_ = base_price;
+                this->listingID_ = listing_id;
+                this->active_ = active; 
+              }
 
     private:
       std::string productName_;
@@ -50,9 +58,12 @@ class Product {
 
 class Technology: public Product{
     public:
+      // Technology(): 
+      // Product("template_Tech", std::vector<Bid> {}, 0.0, Quality::New, false) {}
+
       Technology(const std::string &product_name,
-                 const std::vector<Bid> &list_of_bids, double base_price,
-                 unsigned long listing_id, Quality quality, bool active);
+              const std::vector<Bid> &list_of_bids, double base_price,
+              unsigned long listing_id, Quality quality, bool active);
 
     private:
 
@@ -100,16 +111,19 @@ class Health: public Product{
 
 enum class ProductCategory {Technology, Automotive, Furniture, Recreation, Health};
 
-Product* ProductFactory(ProductCategory pc){
-    switch(pc){
-        case ProductCategory::Technology: return new Technology();
-        case ProductCategory::Automotive: return new Automotive();
-        case ProductCategory::Furniture: return new Furniture();
-        case ProductCategory::Recreation: return new Recreation();
-        case ProductCategory::Health: return new Health();
-        default: return new Product();
+class ProductFactory{
+  public: 
+    static Product* CreateProduct(ProductCategory pc){
+      switch(pc){
+          case ProductCategory::Technology: return new Technology("basic_Tech", {}, 0.0, 0, Quality::New, false);
+          case ProductCategory::Automotive: return new Automotive("basic_Auto", {}, 0.0, 0, Quality::New, false);
+          case ProductCategory::Furniture: return new Furniture("basic_Furn", {}, 0.0, 0, Quality::New, false);
+          case ProductCategory::Recreation: return new Recreation("basic_Rec", {}, 0.0, 0, Quality::New, false);
+          case ProductCategory::Health: return new Health("basic_Heal", {}, 0.0, 0, Quality::New, false);
+          default: return new Product("basic_Prod", {}, 0.0, 0, Quality::New, false);
     }
-}
+  }
+};
     
 
 #endif // _PRODUCT_H_
